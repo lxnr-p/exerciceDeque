453f2cf5f1ca39ce5c19864c4f962c3a9e982b17
Alex <music-and-drawing@hotmail.fr>
2023-07-04 11:31:00 +0200

    first commit

---
+++ exerciceDeque.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <deque>
+
+
+using namespace std;
+
+
+void showDeque(deque<char>)
+{
+    deque<char>::iterator it;
+    int i, j, side;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+}
+
+
+void addPoint(deque<char>)
+{
+    deque<char> deck;
+    std::deque<char>::iterator it = deck.begin();
+    deck.insert(it, 'x');
+}
+
+
+int main()
+{
+    deque<char> deck;
+
+    addPoint(deck);
+
+    showDeque(deck);
+    return 0;
+}
